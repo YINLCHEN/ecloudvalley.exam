bebf8f5cb5e5329dd0b21f8f3e64c51fc386eb15
陳胤霖 <sidneychen137@gmail.com>
2018-06-05 14:40:43 +0800

    rwd

--- src/components/AppbarMenu.js
+++ src/components/AppbarMenu.js
@@ -3,6 +3,13 @@ import { Label, Menu } from 'semantic-ui-react'
 import { connect } from 'react-redux';
 
 class AppbarMenu extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { 
+            menuSize: 'large'
+        };
+    }
+
     state = { activeItem: 'inbox' }
 
     handleItemClick = (e, { name }) => {
@@ -13,11 +20,27 @@ class AppbarMenu extends Component {
         });
     }
 
+    componentDidMount(){
+        window.addEventListener('resize',()=>{
+            var scrollTop = document.body.clientWidth || document.documentElement.clientWidth;
+            
+            if(scrollTop <= 1100){
+                this.setState({menuSize: 'mini'});
+            }
+            else if(scrollTop> 1100 && scrollTop <= 1500){
+                this.setState({menuSize: 'small'});
+            }
+            else if(scrollTop > 1500){
+                this.setState({menuSize: 'large'});
+            }
+        })
+    }
+
     render() {
         const { activeItem } = this.state
 
         return (
-        <Menu size='large' vertical style={{padding:0}}>
+        <Menu size={this.state.menuSize} vertical style={{padding:0}}>
             <Menu.Item name='inbox' active={activeItem === 'inbox'} onClick={this.handleItemClick}>
                 <Label color='orange'>2</Label>
                 Inbox
